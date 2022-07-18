b0ff340ce9351ad0e8d49bc658a15f318120789a
alfianahar <alfian.aswinda@gmail.com>
2022-07-18 22:28:51 +0700

    add display for volume

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,7 @@ function Credit() {
   )
 }
 
-function Control({power, setPower, vol, setVol}) {
+function Control({power, setPower, vol, setVol, display, setDisplay}) {
 
   React.useEffect(() => {
     const input = document.querySelector(".range");
@@ -78,16 +78,32 @@ function Control({power, setPower, vol, setVol}) {
     return size;
   }
   }, []);
+
+  React.useEffect(() => {
+    const volDisp = Math.round(vol*100)
+    function clearDisp(volDisp) {
+      setDisplay('volume: ' + volDisp)
+      setTimeout(() => setDisplay(''), 1000);
+    }
+    
+    clearDisp(volDisp)
+    
+  }, [vol]);
+  
+
   return (
     <div className="flex flex-col items-center justify-center w-full ">
       <label className="flex flex-col items-center cursor-pointer mb-4">
-        <span className='p-2 font-bold text-[#E1E5F2]'>POWER</span>
+        <span className=' font-bold text-[#E1E5F2]'>POWER</span>
         <div className="relative">
           <input type="checkbox" className="sr-only" checked={power} onClick={() => setPower(!power)}/>
-          <div className={`block w-14 h-8 rounded-full ${power ? "bg-gray-600 " : "bg-[#E1E5F2]"}`}></div>
-          <div className={`absolute left-1 top-1 bg-[#1F7A8C] w-6 h-6 rounded-full transition ${power ? "" : "translate-x-full duration-300 ease-in-out"} `}></div>
+          <div className={`block w-14 h-8 rounded-full ${power ? "bg-[#777] " : "bg-[#E1E5F2]"}`}></div>
+          <div className={`absolute left-1 top-1 bg-[#1F7A8C] border border-2 w-6 h-6 rounded-full transition ${power ? "" : "translate-x-full duration-300 ease-in-out"} `}></div>
         </div>
       </label>
+      <div className="p-1 mb-4 border border-2 rounded-md w-3/4 bg-[#E1E5F2] text-center h-9">
+        <span>{display}</span>
+      </div>
       <label className="flex items-center cursor-pointer">
         <input type='range' min={0} max={1} step={0.01} defaultValue={vol} onChange={e => setVol(Number(e.target.value))} className="range" />
       </label>
@@ -98,14 +114,15 @@ function Control({power, setPower, vol, setVol}) {
 function App() {
   const [power, setPower] = React.useState(false)
   const [vol, setVol] = React.useState(0.5)
+  const [display, setDisplay] = React.useState('')
 
   return (
       <div 
         id='drum-machine'
         className='flex flex-col justify-center items-center h-screen bg-none'>
         <Title />
-        <div className="flex flex-col p-6 border rounded-md">
-          <Control power={power} setPower={setPower} vol={vol} setVol={setVol} />
+        <div className="flex flex-col p-6 border rounded-md bg-[#1F7A8C]/70">
+          <Control power={power} setPower={setPower} vol={vol} setVol={setVol} display={display} setDisplay={setDisplay} />
 
         </div>
 
